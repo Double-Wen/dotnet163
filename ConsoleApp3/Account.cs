6bc16c4d1d2f86ca5c96912d67daae068bad092e
Double-Wen <tcliuwenwen@126.com>
2020-05-24 16:41:23 +0800

    fix third homework

--- ConsoleApp3/Account.cs
+++ ConsoleApp3/Account.cs
@@ -1,20 +1,44 @@
 
 public class Account {
 
-    double money; //decimal money;
-	string id;
-	string pwd;
+    private double money; //decimal money;
+	private string id;
+	private string pwd;
+
+	public double Money { get => money; set => money = value; }
+	public string Id { get => id; set => id = value; }
+	public string Pwd { get => pwd; set => pwd = value; }
+
 	//string name;
 	
-	public Account( string id, string pwd, double money )
+	
+	public bool SaveMoney( double money)
 	{
-		//if( money < 0 ) throw new Exception("....");
-		this.id = id;
-		this.pwd = pwd;
-		this.money = money;
+		if( money < 0 ) return false;  //ÎÀÓï¾ä
+		
+		this.money += money;
+		return true;		
 	}
 	
-	public double getMoney()
+	public bool WithdrawMoney( double money)
+	{
+		if( this.money >= money )
+		{
+			this.money -= money;
+			return true;
+		}
+
+		return false;
+
+	}
+	
+	public bool IsMatch( string id, string pwd )
+	{
+		return id==this.id && pwd==this.pwd;
+	}
+	
+	
+		public double getMoney()
 	{
 		return money;
 	}
@@ -43,32 +67,5 @@ public class Account {
 	{
 		this.pwd = pwd;
 	}
-	
-	public bool SaveMoney( double money)
-	{
-		if( money < 0 ) return false;  //ÎÀÓï¾ä
-		
-		this.money += money;
-		return true;		
-	}
-	
-	public bool WithdrawMoney( double money)
-	{
-		if( this.money >= money )
-		{
-			this.money -= money;
-			return true;
-		}
-
-		return false;
-
-	}
-	
-	public bool IsMatch( string id, string pwd )
-	{
-		return id==this.id && pwd==this.pwd;
-	}
-	
-	
 	
 }
--- ConsoleApp3/CreditCardAccount.cs
+++ ConsoleApp3/CreditCardAccount.cs
@@ -4,7 +4,35 @@ using System.Text;
 
 namespace ConsoleApp3
 {
-    class CreditCardAccount
+    class CreditCardAccount:Account
     {
+        private double creditMoney;
+
+        private bool creditEnabled;
+
+        public double CreditMoney { get => creditMoney; set => creditMoney = value; }
+        public bool CreditEnabled { get => creditEnabled; set => creditEnabled = value; }
+        
+        public void disableCredit()
+        {
+            this.CreditEnabled = false;
+            Console.WriteLine(this.CreditEnabled);
+            return;
+        }
+
+        public void enableCredit()
+        {
+            this.CreditEnabled = true;
+            Console.WriteLine(this.CreditEnabled);
+            return;
+        }
+
+        public bool WithdrawMoney(double money)
+        {
+            Console.WriteLine("你是信用卡，不能取钱");
+            return false;
+
+        }
+
     }
 }
